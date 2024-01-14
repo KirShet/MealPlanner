099b290a9e6b25bdcecc4387b9919250f79c8b05
KirShet <116618071+KirShet@users.noreply.github.com>
2024-01-14 17:55:55 +0600

    Updating the page

--- components/planning_meals.php
+++ components/planning_meals.php
@@ -95,7 +95,7 @@
                             </div>
                         </div>
                         <div class="techna">
-                            <form <?= action('send_menu')?>>
+                            <form id="heart_form" class = "heart_form" action = "#heart_form">
                             <input type="submit" name="heart" class="heart" value="">
                             <input type="hidden" name="value" value="<?=$value?>">
                             </form>
